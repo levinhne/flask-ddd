b8f389a5f8d2e3557873ada5c77832d27d75c41d
Your Name <you@example.com>
2024-10-21 13:43:27 +0700

    update code

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, request
+from flask import Blueprint, current_app
 
 auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth_blueprint.route("/logout", methods=["GET"])
 def logout():
+    current_app.logger.info("Logout successfully!")
     return {
         "message": "Logout successfully!"
     }
